8c81b543ab18f0e204b2d0937c00692ea12ac403
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-11-09 15:08:09 -0300

    Add load more function to custom hooks

--- src/hooks/useGetFeed.ts
+++ src/hooks/useGetFeed.ts
@@ -3,9 +3,12 @@ import { useHttpRequestService } from "../service/HttpRequestService";
 import { setLength, updateFeed } from "../redux/user";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 
+const POST_LIMIT = 10;
+
 export const useGetFeed = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
   const posts = useAppSelector((state) => state.user.feed);
   const query = useAppSelector((state) => state.user.query);
 
@@ -14,12 +17,14 @@ export const useGetFeed = () => {
   const service = useHttpRequestService();
 
   useEffect(() => {
+    setHasMore(true);
     try {
       setLoading(true);
       setError(false);
-      service.getPosts(query).then((res) => {
+      service.getPosts(query, POST_LIMIT).then((res) => {
         dispatch(updateFeed(res));
         dispatch(setLength(res.length));
+        if (!res.length) { setHasMore(false) };
         setLoading(false);
       });
     } catch (e) {
@@ -28,5 +33,23 @@ export const useGetFeed = () => {
     }
   }, [query]);
 
-  return { posts, loading, error };
+  const loadMore = async () => {
+    if (posts.length < POST_LIMIT) {
+      setHasMore(false);
+      return;
+    };
+    setLoading(true);
+    try {
+      const morePosts = await service.getPosts(query, POST_LIMIT, posts.length)
+      if (!morePosts.length || morePosts.length < POST_LIMIT) setHasMore(false);
+      dispatch(updateFeed([...posts, ...morePosts]))
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { posts, loading, error, hasMore, loadMore };
 };
