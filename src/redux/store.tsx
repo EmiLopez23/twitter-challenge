6985a0cbc82940ac1db5d9591f1e456934a99045
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-11-13 16:52:19 -0300

    add user and token to whitelist

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -6,6 +6,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage,
+  whitelist: ["user", "token"]
 }
 
 export const persistedReducer = persistReducer(persistConfig, user)
